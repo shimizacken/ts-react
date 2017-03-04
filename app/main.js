205f9a3f467ea9cd76df629ba3176b27c5dfcf07
Shimi Zacken <shimizacken@gmail.com>
2017-03-04 16:04:53 +0200

    typed react added.  webpack.config support tsx files

--- app/main.js
+++ app/main.js
@@ -2,6 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 
-
-
 ReactDOM.render(React.createElement('div', null, 'Hello world'), document.getElementById('divMainContainer'));
--- webpack.config.js
+++ webpack.config.js
@@ -7,11 +7,28 @@ module.exports = {
         path: path.resolve(__dirname, 'build'),
         filename: 'bundle.js'
     },
+    devtool: "source-map",
+    resolve: {
+        // Add '.ts' and '.tsx' as resolvable extensions.
+        extensions: [".webpack.js", ".web.js", ".ts", ".tsx", ".js"]
+    },
      //plugins: [
         //new webpack.optimize.CommonsChunkPlugin({ name: "vendor", filename: "vendor.bundle.js" }),
         //new HappyPack({ loaders: ['babel-loader?presets[]=es2015']}),
     //],
      module: {
+         rules: [
+             {
+                 test: /\.tsx?$/,
+                 loader: "awesome-typescript-loader"
+             },
+             {
+                 test: /\.js$/,
+                 enforce: "pre",
+                 loader: "source-map-loader"
+             }
+         ]
+         /*
          loaders: [
              {
                 test: /\.jsx?$/,
@@ -21,7 +38,7 @@ module.exports = {
                     presets: ['es2015'],
                 }
              }
-         ]
+         ]*/
      },
      devServer: {
          inline: true,
